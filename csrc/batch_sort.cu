6c68858fbbd74adc48a97524b543ee08b72ccc18
lei.jiabao <karbolei@outlook.com>
2021-04-17 18:32:36 +0800

    batch_sort supports both CPU(tbb) and CUDA

--- csrc/batch_sort.cu
+++ csrc/batch_sort.cu
@@ -4,23 +4,37 @@
 #include <thrust/sort.h>
 #include <thrust/device_ptr.h>
 #include <thrust/iterator/zip_iterator.h>
+#include <thrust/execution_policy.h>
 
 #include <pybind11/pybind11.h>
 namespace py = pybind11;
 
 /////////////////////////////////////////////////////////////////////////////
 
-template <typename FloatType, typename IndexType, typename SizeType, bool Increasing>
+template <typename FloatType, typename IndexType, typename SizeType, bool Increasing, bool CUDA>
 void batchSort_kernel(FloatType *value, IndexType *batch, IndexType *index_out, SizeType length)
 {   
-    thrust::device_ptr<FloatType> value_ptr = thrust::device_pointer_cast(value);
-    thrust::device_ptr<IndexType> batch_ptr = thrust::device_pointer_cast(batch);
-    thrust::device_ptr<IndexType> index_ptr = thrust::device_pointer_cast(index_out);
+    if constexpr (CUDA)
+    {
+        thrust::device_ptr<FloatType> value_ptr = thrust::device_pointer_cast(value);
+        thrust::device_ptr<IndexType> batch_ptr = thrust::device_pointer_cast(batch);
+        thrust::device_ptr<IndexType> index_ptr = thrust::device_pointer_cast(index_out);
 
-    auto first = thrust::make_zip_iterator(thrust::make_tuple(batch_ptr, index_ptr));
-    if constexpr (Increasing) thrust::stable_sort_by_key(value_ptr, value_ptr + length, first, thrust::less<FloatType>());
-    else thrust::stable_sort_by_key(value_ptr, value_ptr + length, first, thrust::greater<FloatType>());
-    thrust::stable_sort_by_key(batch_ptr, batch_ptr + length, index_ptr, thrust::less<IndexType>());
+        auto first = thrust::make_zip_iterator(thrust::make_tuple(batch_ptr, index_ptr));
+        if constexpr (Increasing) thrust::stable_sort_by_key(thrust::device, value_ptr, value_ptr + length, first, thrust::less<FloatType>());
+        else thrust::stable_sort_by_key(thrust::device,value_ptr, value_ptr + length, first, thrust::greater<FloatType>());
+        thrust::stable_sort_by_key(thrust::device, batch_ptr, batch_ptr + length, index_ptr, thrust::less<IndexType>());
+    } else
+    {
+        FloatType *value_ptr = value;
+        IndexType *batch_ptr = batch;
+        IndexType *index_ptr = index_out;
+
+        auto first = thrust::make_zip_iterator(thrust::make_tuple(batch_ptr, index_ptr));
+        if constexpr (Increasing) thrust::stable_sort_by_key(thrust::host, value_ptr, value_ptr + length, first, thrust::less<FloatType>());
+        else thrust::stable_sort_by_key(thrust::host, value_ptr, value_ptr + length, first, thrust::greater<FloatType>());
+        thrust::stable_sort_by_key(thrust::host, batch_ptr, batch_ptr + length, index_ptr, thrust::less<IndexType>());
+    }
 }
 
 
@@ -32,8 +46,15 @@ void batchSort(torch::Tensor value, torch::Tensor batch, torch::Tensor index_out
     IndexType* index_out_ptr = index_out.data_ptr<IndexType>();
     SizeType   length        = value.size(0);
 
-    if (increasing) batchSort_kernel<FloatType, IndexType, SizeType, true>(value_ptr, batch_ptr, index_out_ptr, length);
-    else batchSort_kernel<FloatType, IndexType, SizeType, false>(value_ptr, batch_ptr, index_out_ptr, length);
+    if (value.is_cuda())
+    {
+        if (increasing) batchSort_kernel<FloatType, IndexType, SizeType, true, true>(value_ptr, batch_ptr, index_out_ptr, length);
+        else batchSort_kernel<FloatType, IndexType, SizeType, false, true>(value_ptr, batch_ptr, index_out_ptr, length);
+    } else
+    {
+        if (increasing) batchSort_kernel<FloatType, IndexType, SizeType, true, false>(value_ptr, batch_ptr, index_out_ptr, length);
+        else batchSort_kernel<FloatType, IndexType, SizeType, false, false>(value_ptr, batch_ptr, index_out_ptr, length);
+    }
 }
 
 
--- src/batch_sort.py
+++ src/batch_sort.py
@@ -14,6 +14,7 @@ def batch_sort(value, batch, increasing=True):
     Returns:
         index (tensor): the index to sort
     """
+    assert value.device == batch.device, "`value` and `batch` must be on the same device!"
     index_out = torch.arange(len(batch), device=batch.device)
     batch_sort_kernel(value.clone(), batch.clone(), index_out, increasing) # change in place
     return index_out
@@ -21,12 +22,14 @@ def batch_sort(value, batch, increasing=True):
 
 
 if __name__ == "__main__": # CUDA_VISIBLE_DEVICES=6 python batch_sort.py
-    value = torch.rand(100).cuda()
-    batch = torch.repeat_interleave(torch.arange(3), torch.tensor([30, 65, 5])).cuda()
-    print(f"value = {value}")
-    print(f"batch = {batch}")
-    index = batch_sort(value, batch, increasing=True)
-    print(f"index = {index}")
-    print(f"sorted value = {value[index]}")
+    for device in ["cuda", "cpu"]:
+        print(f"====== device = {device} ======")
+        value = torch.rand(100).to(device=device)
+        batch = torch.repeat_interleave(torch.arange(3), torch.tensor([30, 65, 5])).to(device=device)
+        print(f"value = {value}")
+        print(f"batch = {batch}")
+        index = batch_sort(value, batch, increasing=True)
+        print(f"index = {index}")
+        print(f"sorted value = {value[index]}")
+        print(f"===============================")
 
-    import ipdb; ipdb.set_trace()
